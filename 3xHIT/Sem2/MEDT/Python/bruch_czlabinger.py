92f0e3c5791d9a3430b2ae5bfc47139b2ac31d12
czlabinger <czlabinger@student.tgm.ac.at>
2023-03-07 14:21:05 +0100

    MEDT bruch

--- 3xHIT/Sem2/MEDT/Python/bruch_czlabinger.py
+++ 3xHIT/Sem2/MEDT/Python/bruch_czlabinger.py
@@ -3,19 +3,82 @@ class Bruch():
         self.number1 = number1
         self.number2 = number2
 
+    # +
     def __add__(self, b2):
         top = self.number1 * b2.number2 + b2.number1 * self.number2
         bottom = self.number2 * b2.number2
         return "(" + top.__str__() + "/" + bottom.__str__() + ")"
 
+    # -
     def __sub__(self, b2):
         top = self.number1 * b2.number2 - b2.number1 * self.number2
         bottom = self.number2 * b2.number2
         return "(" + top.__str__() + "/" + bottom.__str__() + ")"
 
+    # *-1
     def __neg__(self):
-        self.number1 = self.number1
-        return "(" + self.number1.__str__() + "/" + self.number2.__str__() + ")"
+        self.number1 = self.number1 * -1
+        return "("+self.number1.__str__()+"/"+self.number2.__str__()+")"
+
+    # *
+    def __mul__(self, b2):
+        top = self.number1 * b2.number1
+        bottom = self.number2 * b2.number2
+        return "(" + top.__str__() + "/" + bottom.__str__() + ")"
+
+    # Invert
+    def __invert__(self):
+        top = self.number1
+        self.number1 = self.number2
+        self.number2 = top
+        return "("+self.number1.__str__()+"/"+self.number2.__str__()+")"
+
+    # /
+    def __truediv__(self, b2):
+        ~b2
+        return self * b2
+
+    # <
+    def __lt__(self, b2):
+        top1 = self.number1 * b2.number2
+        top2 = b2.number1 * self.number2
+
+        return top1 < top2
+
+    # <=
+    def __le__(self, b2):
+        top1 = self.number1 * b2.number2
+        top2 = b2.number1 * self.number2
+
+        return top1 <= top2
+
+    # >
+    def __gt__(self, b2):
+        top1 = self.number1 * b2.number2
+        top2 = b2.number1 * self.number2
+
+        return top1 > top2
+
+    # >=
+    def __ge__(self, b2):
+        top1 = self.number1 * b2.number2
+        top2 = b2.number1 * self.number2
+
+        return top1 >= top2
+
+    # ==
+    def __eq__(self, b2):
+        top1 = self.number1 * b2.number2
+        top2 = b2.number1 * self.number2
+
+        return top1 == top2
+
+    # !=
+    def __ne__(self, b2):
+        top1 = self.number1 * b2.number2
+        top2 = b2.number1 * self.number2
+
+        return top1 != top2
 
 
 def main():
@@ -24,6 +87,15 @@ def main():
     print(b1 + b2)
     print(b1 - b2)
     print(-b1)
+    print(b1 * b2)
+    print(~b1)
+    print(b1 / b2)
+    print(b2 < b1)
+    print(b2 <= b1)
+    print(b1 > b2)
+    print(b1 >= b2)
+    print(b1 == b2)
+    print(b1 != b2)
 
 
 if __name__ == "__main__":
