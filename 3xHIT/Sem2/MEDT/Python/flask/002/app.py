19f4b5b5ec36436902910f9e9b99dbed9bac1f03
czlabinger <czlabinger@student.tgm.ac.at>
2023-04-24 22:12:45 +0200

    ITP and MEDT

--- 3xHIT/Sem2/MEDT/Python/flask/002/app.py
+++ 3xHIT/Sem2/MEDT/Python/flask/002/app.py
@@ -3,10 +3,19 @@ from flask import render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 @app.route('/index')
-
 def home():
-    users["Christof"] = True
-    users["Flo"] = False
-    return render_template('index.html', title='Welcome', users)
+    users = User()
+
+    return render_template('index.html', title='Welcome', users=users)
+
+
+class User:
+
+    def __init__(self):
+        dict = {
+            "Christof": True,
+            "Matthias": False
+        }
