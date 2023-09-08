9a2ce47c132d11f31932e22c3e8532bcf0c52d9d
czlabinger <czlabinger@student.tgm.ac.at>
2023-09-08 19:14:55 +0200

    SEW android eventhandling done

--- 4xHIT/SEW/Eventhandling/app/src/main/java/at/czlabinger/eventhandling/MainActivity.java
+++ 4xHIT/SEW/Eventhandling/app/src/main/java/at/czlabinger/eventhandling/MainActivity.java
@@ -1,18 +1,15 @@
 package at.czlabinger.eventhandling;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private ImageView imageView;
-    private int xDelta, yDelta;
+    private float x, y;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,29 +18,27 @@ public class MainActivity extends AppCompatActivity {
 
         imageView = findViewById(R.id.img);
 
-        imageView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                final int x = (int) event.getRawX();
-                final int y = (int) event.getRawY();
-                Log.i("a", String.valueOf(x));
-
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-                    case MotionEvent.ACTION_DOWN:
-                        view.setX(x);
-                        view.setY(y);
-                        break;
-                    case MotionEvent.ACTION_UP:
-                        view.setX(x);
-                        view.setY(y);
-                        break;
-                    case MotionEvent.ACTION_MOVE:
-                        view.setX(x);
-                        view.setY(y);
-                        break;
-                }
-                return true;
-            }
-        });
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction() == MotionEvent.ACTION_DOWN) {
+            x = event.getX();
+            y = event.getY();
+        }
+
+        if(event.getAction() == MotionEvent.ACTION_MOVE) {
+            float dx = event.getX() - x;
+            float dy = event.getY() - y;
+
+            imageView.setX(imageView.getX() + dx);
+            imageView.setY((imageView.getY() + dy));
+
+            x = event.getX();
+            y = event.getY();
+        }
+
+        return super.onTouchEvent(event);
+    }
+
 }
