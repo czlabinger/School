0968ff5210dda8fa3a73e3ed6e848668693c1546
czlabinger <czlabinger@student.tgm.ac.at>
2024-03-05 16:24:57 +0100

    I fucking hate this shit

--- 4xHIT/DS/gundlagenDesMaschinellenLernens/grundlagenDesMaschinellenLernens.py
+++ 4xHIT/DS/gundlagenDesMaschinellenLernens/grundlagenDesMaschinellenLernens.py
@@ -3,6 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from sklearn.preprocessing import PolynomialFeatures
 
 """
 Aufgabe 1
@@ -10,16 +11,18 @@ Teil 1
 """
 economics = pd.read_csv('economics.csv')
 
-x = economics["uempmed"].values.reshape(-1, 1)
-y = economics["unemploy"].values.reshape(-1, 1)
+unempmed = economics["uempmed"].values.reshape(-1, 1)
+unemploy = economics["unemploy"].values.reshape(-1, 1)
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
+
+unempmed_train, unempmed_test, unemploy_train, unemploy_test = train_test_split(unempmed, unemploy, test_size=0.2,
+                                                                                random_state=0)
 
 model1: LinearRegression = LinearRegression()
 model2: LinearRegression = LinearRegression()
 
-model1.fit(x_train, y_train)
-model2.fit(y_train, x_train)
+model1.fit(unempmed_train, unemploy_train)
+model2.fit(unemploy_train, unempmed_train)
 
 # Gespiegelt da Achsen vertauscht wurden
 
@@ -28,8 +31,8 @@ Teil 2
 """
 # region Linear 1
 
-plt.scatter(x_train, y_train, label="Data")
-plt.plot(x_train, model1.predict(x_train), color="red", label="Linear Regression")
+plt.scatter(unempmed_train, unemploy_train, label="Data")
+plt.plot(unempmed_train, model1.predict(unempmed_train), color="red", label="Linear Regression")
 plt.xlabel("uempmed")
 plt.ylabel("unemploy")
 plt.show()
@@ -38,8 +41,8 @@ plt.show()
 
 # region Linear 2
 
-plt.scatter(y_train, x_train, label="Data")
-plt.plot(y_train, model2.predict(y_train), color="red", label="Linear Regression")
+plt.scatter(unemploy_train, unempmed_train, label="Data")
+plt.plot(unemploy_train, model2.predict(unemploy_train), color="red", label="Linear Regression")
 plt.ylabel("uempmed")
 plt.xlabel("unemploy")
 plt.show()
@@ -50,44 +53,80 @@ plt.show()
 Teil 3
 """
 
+# region Log
+
+log: LinearRegression = LinearRegression()
+unempmed_train_log = np.log(unempmed_train)
+
+log.fit(unempmed_train_log, unemploy_train)
+
+print(log.intercept_, log.coef_, log.score(unempmed_train_log, unemploy_train))
+
+# endregion
 
 # region Exp
 
-new_x = np.exp(x)
+exp: LinearRegression = LinearRegression()
+unempmed_train_exp = np.exp(unempmed_train)
 
-coef = np.polyfit(new_x.flatten(), y.flatten(), 1)
-exp1 = np.poly1d(coef)
+exp.fit(unemploy_train, unempmed_train_log)
 
-plt.plot(np.log(new_x), y, 'o', np.log(new_x), exp1(new_x), '--k')
-plt.show()
+print(exp.intercept_, log.coef_, exp.score(unemploy_train, unempmed_train_log))
 
 # endregion
+
+"""
 
 # region Log
 
 log: LinearRegression = LinearRegression()
-x_train_log = np.log(x_train)
-y_train_log = np.log(y_train)
+unempmed_train_log = np.log(unempmed_train)
 
-log.fit(y_train_log, x_train_log)
+log.fit(unempmed_train_log, unemploy_train)
 
-plt.scatter(y_train, x_train, label="Data")
-plt.plot(y_train, np.exp(log.predict(y_train_log)), color="red", label="Linear Regression")
+plt.scatter(unempmed_train, unemploy_train, label="Data")
+plt.plot(unempmed_train, log.predict(unemploy_train), color="red", label="Linear Regression")
+plt.xlabel("uempmed")
+plt.ylabel("unemploy")
+plt.show()
+
+# endregion
+
+# region Exp
+
+exp: LinearRegression = LinearRegression()
+unempmed_train_exp = np.exp(unempmed_train)
+
+exp.fit(y_train, x_train_exp)
+
+plt.scatter(y_train, x_train_exp, label="Data")
+plt.plot(exp.predict(x_train_exp), x_train, color="red", label="Linear Regression")
 plt.ylabel("uempmed")
 plt.xlabel("unemploy")
 plt.show()
 
 # endregion
+"""
 
 """
 Teil 5
 """
 
-pol_model = np.poly1d(np.polyfit(x.flatten(), y.flatten(), 10))
+# region pol
 
-plt.scatter(x, y, label="original daten")
-plt.plot(x, y, 'o', x, pol_model(x), '--k')
+poly = PolynomialFeatures(degree=10)
+x_poly = poly.fit_transform(unempmed.reshape(-1, 1))
+
+x_train, x_test, y_train, y_test = train_test_split(x_poly, unemploy, test_size=0.2, random_state=0)
+
+poly_reg_model = LinearRegression()
+poly_reg_model.fit(x_train, y_train)
+
+plt.scatter(unempmed, unemploy, color='blue')
+plt.plot(unempmed, poly_reg_model.predict(x_poly), color='red')
 plt.show()
+
+# endregion
 
 """
 Teil 6
