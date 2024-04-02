e7e52d14c05551d3d336535817f6727d05a52273
czlabinger <czlabinger@student.tgm.ac.at>
2024-04-02 18:03:12 +0200

    Industielle Programierung

---
+++ 4xHIT/SYT/IndPro/schieber/stuppnig_gmbh.h
@@ -0,0 +1,38 @@
+#ifndef CUSTOM_PDO_NAME_H
+#define CUSTOM_PDO_NAME_H
+
+//-------------------------------------------------------------------//
+//                                                                   //
+//     This file has been created by the Easy Configurator tool      //
+//                                                                   //
+//     Easy Configurator project stuppnig_gmbh.prj
+//                                                                   //
+//-------------------------------------------------------------------//
+
+
+#define CUST_BYTE_NUM_OUT	1
+#define CUST_BYTE_NUM_IN	1
+#define TOT_BYTE_NUM_ROUND_OUT	4
+#define TOT_BYTE_NUM_ROUND_IN	4
+
+
+typedef union												//---- output buffer ----
+{
+	uint8_t  Byte [TOT_BYTE_NUM_ROUND_OUT];
+	struct
+	{
+		uint8_t     lieferPlatine;
+	}Cust;
+} PROCBUFFER_OUT;
+
+
+typedef union												//---- input buffer ----
+{
+	uint8_t  Byte [TOT_BYTE_NUM_ROUND_IN];
+	struct
+	{
+		uint8_t     platineDa;
+	}Cust;
+} PROCBUFFER_IN;
+
+#endif
